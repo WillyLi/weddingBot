9ae19638d2cce420648253cc04d21a6957e9b0ec
Willy Li <willy.li@linecorp.com>
2019-10-30 19:31:44 +0800

    Add no table case

--- api/linebotParser.js
+++ api/linebotParser.js
@@ -10,6 +10,31 @@ bot.on('message', function(event) {
   console.log(event.message.type)
   if (event.message.type == 'text') {
     const table = guestMap[event.message.text.trim()]
+    if (!table) {
+      event
+        .reply(
+          `對不起，找不到您的名字，請輸入全名(EX:王小明)，或由招待人員為您服務。`
+        )
+        .then(function(data) {
+          // success
+          console.log(msg)
+        })
+        .catch(function(error) {
+          // error
+          console.log('error')
+        })
+      return
+    }
+    event
+      .reply(`您的座位在${table}著`)
+      .then(function(data) {
+        // success
+        console.log(msg)
+      })
+      .catch(function(error) {
+        // error
+        console.log('error')
+      })
     event
       .reply({
         type: 'image',
