6dfa4a0bc01c126c0eb4c644976860bd1eb620ec
Willy Li <willy.li@linecorp.com>
2019-10-30 13:30:18 +0800

    hide googleKey

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  googleKey: '"AIzaSyBOpRUW2VMaZ9D9TEVKQSZ_nQCUKcCyGLE"',
   imagePlaceholder: '"https://i1.wp.com/www.foot.com/wp-content/uploads/2017/03/placeholder.gif?zoom=2&fit=300%2C185&ssl=1"',
   googlePlaceApiUrl: '"https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  googleKey: '"AIzaSyBOpRUW2VMaZ9D9TEVKQSZ_nQCUKcCyGLE"',
   imagePlaceholder: '"https://i1.wp.com/www.foot.com/wp-content/uploads/2017/03/placeholder.gif?zoom=2&fit=300%2C185&ssl=1"',
   googlePlaceApiUrl: '"https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json"'
 }
