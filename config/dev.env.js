27bac77197e3f7be92613d3e7ab13132c5af27ff
Willy Li <willy.li@linecorp.com>
2019-10-30 14:49:35 +0800

    Use nuxt init

--- config/dev.env.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  imagePlaceholder: '"https://i1.wp.com/www.foot.com/wp-content/uploads/2017/03/placeholder.gif?zoom=2&fit=300%2C185&ssl=1"',
-  googlePlaceApiUrl: '"https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json"'
-})
--- config/prod.env.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  imagePlaceholder: '"https://i1.wp.com/www.foot.com/wp-content/uploads/2017/03/placeholder.gif?zoom=2&fit=300%2C185&ssl=1"',
-  googlePlaceApiUrl: '"https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json"'
-}
