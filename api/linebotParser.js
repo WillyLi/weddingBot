507b1786e52aac25bf60cba899cb9592bc3eaa8e
Willy Li <willy.li@linecorp.com>
2019-10-31 14:30:51 +0800

    Reply array

--- api/linebotParser.js
+++ api/linebotParser.js
@@ -7,7 +7,7 @@ const bot = linebot({
   channelAccessToken
 })
 bot.on('message', function(event) {
-  const userId = event.userId
+  // const userId = event.userId
   console.log(event.message.type)
   if (event.message.type == 'text') {
     const table = guestMap[event.message.text.trim()]
@@ -27,14 +27,15 @@ bot.on('message', function(event) {
       return
     }
     const msg = `您的座位在${table}桌`
-    bot.push(userId, msg)
-    bot
-      .push(userId, {
-        type: 'image',
-        url: 'https://wedding-line-bot.herokuapp.com/map.jpg',
-        aspectMode: 'cover',
-        size: 'full'
-      })
+    event
+      .reply([
+        msg,
+        {
+          type: 'image',
+          originalContentUrl: 'https://wedding-line-bot.herokuapp.com/map.jpg',
+          previewImageUrl: 'https://wedding-line-bot.herokuapp.com/map.jpg'
+        }
+      ])
       .then(function(data) {
         // success
         console.log('success', data)
