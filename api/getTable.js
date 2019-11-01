88ef34cd217d0b98107874dee70e9dab41920ebb
Willy Li <willy.li@linecorp.com>
2019-11-02 00:55:00 +0800

    Fix typo

--- api/getTable.js
+++ api/getTable.js
@@ -1,5 +1,5 @@
 const guestMap = require('./guestMap.json')
-module.exports = (name) {
+module.exports = (name) => {
   const _name = name.trim()
   for (key in guestMap){
     if(guestMap.includes(_name)){
